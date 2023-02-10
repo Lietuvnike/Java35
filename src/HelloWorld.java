99ff266553ef00f0a42ec72546c7ebbfef32b472
Viktorija Lietuvnike <v.aurylaite@gmail.com>
2023-02-10 21:33:39 +0200

    Variables and data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println();
+        System.out.println("Hello She Goes Tech");
+
+        //SThis is single-line comment();
+        /*
+        This is
+        Multi-line
+        Comment
+         */
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaring new variable of type int (integer)
+        int number;
+
+        //Initialization of numer variable
+        number = 10;
+
+        //Accessing number variable value
+        System.out.println(number);
+
+        //Declaring and Initializing new variable in one line
+        int age = 75;
+        System.out.println("age = " + age);
+
+    }
+}
