e740b3aa315265111dabfa23d916db1ce6f05d09
Viktorija Lietuvnike <v.aurylaite@gmail.com>
2023-04-04 20:44:14 +0300

    grocery list and main method filled regarding april 3 lesson

--- src/tasks/grocery/GroceryList.java
+++ src/tasks/grocery/GroceryList.java
@@ -27,11 +27,41 @@ public class GroceryList {
         }
     }
 
-    //emove Grocery item
+    //Remove Grocery item. We will pass a name of the product
     public void removeGroceryItem(String item){
-
+        int position = findItem(item);
+        if (position >=0 ){
+            groceryList.remove(position);
+            System.out.println(item + " was deleted");
+        }else {
+            System.out.println("Item is not in the list");
+        }
     }
-    //Returning intems index
 
+    //Modify grocery list item. In which place and with whom we want to replace.
+    public void modifyGroceryItem(int position, String newItem){
+        if (position >= 0 && position < groceryList.size()){
+            groceryList.set(position, newItem);
+            System.out.println("Grocery item was modified");
+        }else {
+            System.out.println("Items position is not valid");
+        }
+    }
+
+    //Search item in the list. return with true or false
+    public boolean searchItem(String searchItem){
+        return findItem(searchItem) >=0;
+    }
+
+    //Delete all items
+    public void deleteAllItems(){
+        groceryList.clear();
+        System.out.println("All items in the lit are deleted");
+    }
+
+    //Returning items index. Separate method because we will use in other methods as well
+    private int findItem(String searchItem){
+        return groceryList.indexOf(searchItem);
+    }
 
 }
